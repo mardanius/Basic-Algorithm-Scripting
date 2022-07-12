9bef95b9e6022ce9d0020640abd3a2e336a217b1
mardanius <mariojeda.mg@gmail.com>
2022-07-12 09:34:39 -0400

    Factorization using For Loop

--- basic-algorithm-scripting.js
+++ basic-algorithm-scripting.js
@@ -16,3 +16,17 @@ function reverse2(str){
     return str.split("").reverse().join("");
 }
 console.log(reverse2('Hola'));
+
+/*===========
+Factorization
+===========*/
+
+function factorialize(num) {
+    let fac = 1;
+    for (let i = 1; i <= num; i++){
+      fac = fac * i;
+    }
+    return fac;
+  }
+  
+  console.log(factorialize(5));
